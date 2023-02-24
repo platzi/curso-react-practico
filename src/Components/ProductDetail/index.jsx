41d2d3eae7a3bd416be1e61dfb7ed839f1930b2f
teffcode <estefanyaguilar@lahaus.com>
2023-02-24 14:37:51 -0500

    add: clase 10

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+const ProductDetail = () => {
+  return (
+    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+      <div className='flex justify-between items-center p-6'>
+        <h2 className='font-medium text-xl'>Detail</h2>
+        <div>x</div>
+      </div>
+    </aside>
+  )
+}
+
+export default ProductDetail
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
+import ProductDetail from '../../Components/ProductDetail'
 
 function Home() {
   const [items, setItems] = useState(null)
@@ -21,6 +22,7 @@ function Home() {
           ))
         }
       </div>
+      <ProductDetail />
     </Layout>
   )
 }
