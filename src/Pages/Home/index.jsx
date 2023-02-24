16b7d9b1787e735ea7d4802c2489ea013ea45b8b
teffcode <estefanyaguilar@lahaus.com>
2023-02-24 10:01:51 -0500

    add: clase 7

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
