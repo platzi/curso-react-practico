4cf47a2f319f09611afe4788d09162c10f233f9e
teffcode <estefanyaguilar@lahaus.com>
2023-02-24 15:08:00 -0500

    add: clase 12

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext)
+
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+    <aside
+      className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
         <div>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,11 +4,17 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
   const [count, setCount] = useState(0)
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
 
   return (
     <ShoppingCartContext.Provider value={{
       count,
-      setCount
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen
     }}>
       {children}
     </ShoppingCartContext.Provider>
