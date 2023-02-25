29a462d4cb590d5274a4830f4d86ad851f76854a
teffcode <estefanyaguilar@lahaus.com>
2023-02-25 13:09:21 -0500

    add: clase 24

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -9,8 +9,8 @@ function MyOrders() {
 
   return (
     <Layout>
-      <div className='flex items-center justify-center relative w-80'>
-        <h1>My Orders</h1>
+      <div className='flex items-center justify-center relative w-80 mb-4'>
+        <h1 className='font-medium text-xl'>My Orders</h1>
       </div>
       {
         context.order.map((order, index) => (
