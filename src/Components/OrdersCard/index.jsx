21e2d8b449f15bacd6e48e74a5f402b4b9b578c2
teffcode <estefanyaguilar@lahaus.com>
2023-02-25 11:36:05 -0500

    add: clase 22

---
+++ src/Components/OrdersCard/index.jsx
@@ -0,0 +1,17 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
+
+const OrdersCard = props => {
+  const { totalPrice, totalProducts } = props
+
+  return (
+    <div className="flex justify-between items-center mb-3 border border-black">
+      <p>
+        <span>01.02.23</span>
+        <span>{totalProducts}</span>
+        <span>{totalPrice}</span>
+      </p>
+    </div>
+  )
+}
+
+export default OrdersCard
--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 import OrderCard from '../../Components/OrderCard'
@@ -8,7 +10,12 @@ function MyOrder() {
 
   return (
     <Layout>
-      MyOrder
+      <div className='flex items-center justify-center relative w-80 mb-6'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer' />
+        </Link>
+        <h1>My Order</h1>
+      </div>
       <div className='flex flex-col w-80'>
         {
           context.order?.slice(-1)[0].products.map(product => (
