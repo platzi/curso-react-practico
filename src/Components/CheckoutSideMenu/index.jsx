af15c3b6e20390c94d17408375ad86e9ac80b622
teffcode <estefanyaguilar@lahaus.com>
2023-02-25 10:02:18 -0500

    add: clase 20

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -13,6 +13,18 @@ const CheckoutSideMenu = () => {
     context.setCartProducts(filteredProducts)
   }
 
+  const handleCheckout = () => {
+    const orderToAdd = {
+      date: '01.02.23',
+      products: context.cartProducts,
+      totalProducts: context.cartProducts.length,
+      totalPrice: totalPrice(context.cartProducts)
+    }
+
+    context.setOrder([...context.order, orderToAdd])
+    context.setCartProducts([])
+  }
+
   return (
     <aside
       className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -24,7 +36,7 @@ const CheckoutSideMenu = () => {
             onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
       </div>
-      <div className='px-6 overflow-y-scroll'>
+      <div className='px-6 overflow-y-scroll flex-1'>
         {
           context.cartProducts.map(product => (
             <OrderCard
@@ -38,11 +50,12 @@ const CheckoutSideMenu = () => {
           ))
         }
       </div>
-      <div className='px-6'>
-        <p className='flex justify-between items-center'>
+      <div className='px-6 mb-6'>
+        <p className='flex justify-between items-center mb-2'>
           <span className='font-light'>Total:</span>
           <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
         </p>
+        <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
       </div>
     </aside>
   )
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -22,6 +22,9 @@ export const ShoppingCartProvider = ({children}) => {
   // Shopping Cart · Add products to cart
   const [cartProducts, setCartProducts] = useState([])
 
+  // Shopping Cart · Order
+  const [order, setOrder] = useState([])
+
   return (
     <ShoppingCartContext.Provider value={{
       count,
@@ -35,7 +38,9 @@ export const ShoppingCartProvider = ({children}) => {
       setCartProducts,
       isCheckoutSideMenuOpen,
       openCheckoutSideMenu,
-      closeCheckoutSideMenu
+      closeCheckoutSideMenu,
+      order,
+      setOrder
     }}>
       {children}
     </ShoppingCartContext.Provider>
