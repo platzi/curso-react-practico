f923ead0dd3acd07303631a55ab18a346570cfb7
teffcode <estefanyaguilar@lahaus.com>
2023-02-25 08:51:59 -0500

    add: clase 18

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -7,6 +7,11 @@ import './styles.css'
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
 
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id != id)
+    context.setCartProducts(filteredProducts)
+  }
+
   return (
     <aside
       className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -23,9 +28,11 @@ const CheckoutSideMenu = () => {
           context.cartProducts.map(product => (
             <OrderCard
               key={product.id}
+              id={product.id}
               title={product.title}
               imageUrl={product.images}
               price={product.price}
+              handleDelete={handleDelete}
             />
           ))
         }
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,7 +1,7 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = props => {
-  const { title, imageUrl, price } = props
+  const { id, title, imageUrl, price, handleDelete } = props
 
   return (
     <div className="flex justify-between items-center mb-3">
@@ -13,7 +13,7 @@ const OrderCard = props => {
       </div>
       <div className='flex items-center gap-2'>
         <p className='text-lg font-medium'>{price}</p>
-        <XMarkIcon className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
+        <XMarkIcon onClick={() => handleDelete(id)} className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
       </div>
     </div>
   )
