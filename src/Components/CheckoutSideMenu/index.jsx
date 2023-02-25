ff3d8f319899a6952fb776936a3ce1c665a0b4fd
teffcode <estefanyaguilar@lahaus.com>
2023-02-25 09:22:15 -0500

    add: clase 19

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import OrderCard from '../../Components/OrderCard'
+import { totalPrice } from '../../utils'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -36,6 +37,12 @@ const CheckoutSideMenu = () => {
             />
           ))
         }
+      </div>
+      <div className='px-6'>
+        <p className='flex justify-between items-center'>
+          <span className='font-light'>Total:</span>
+          <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
+        </p>
       </div>
     </aside>
   )
