1100c403c7955403b2f73961ec9724456a11c5df
teffcode <estefanyaguilar@lahaus.com>
2023-03-08 18:36:17 -0500

    add: lab reto 1

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,6 +7,12 @@ const Navbar = () => {
   const context = useContext(ShoppingCartContext)
   const activeStyle = 'underline underline-offset-4'
 
+  const handleSignOut = () => {
+    const stringifiedSignOut = JSON.stringify(true)
+    localStorage.setItem('sign-out', stringifiedSignOut)
+    context.setSignOut(true)
+  }
+
   return (
     <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
       <ul className='flex items-center gap-3'>
@@ -103,8 +109,9 @@ const Navbar = () => {
             to='/sing-in'
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
-            }>
-            Sign In
+            }
+            onClick={() => handleSignOut()}>
+            Sign out
           </NavLink>
         </li>
         <li className='flex items-center'>
