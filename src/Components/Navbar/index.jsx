b37721f208b6550dfd399ea492bd43fd55fac3ef
teffcode <estefanyaguilar@lahaus.com>
2023-03-08 23:37:11 -0500

    add: lab reto 10

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white'>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink to={`${isUserSignOut ? '/sign-in' : '/'}`}>
