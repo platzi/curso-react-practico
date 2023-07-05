06f745f53d37c69cf330f05af9417bb72f098248
Diego Portillo <djportillo@taxit.com.py>
2023-07-05 11:57:50 -0400

    Se corrige el enlace del navbar. Se agregan las maquetas de MyAccount, Signin y Signup.

--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -3,7 +3,17 @@ import Layout from '../../Components/Layout'
 function MyAccount() {
   return (
     <Layout>
-      MyAccount
+      <div className='flex flex-col text-center w-72'>
+        <div className='py-2 font-medium text-xl'>  
+          My Account
+        </div>
+        <div className='py-3'>
+          <div className='text-left'><span className='opacity-50 '>Name: </span><span>test</span></div>
+          <div className='text-left'><span className='opacity-50 '>Email: </span><span></span>test@test.com</div>
+        </div>
+        <div className='py-3 cursor-pointer rounded-lg border-2 border-black mb-3'>Edit</div>
+        <div className='py-3 bg-black text-white cursor-pointer rounded-lg'>Log Out</div>
+      </div>
     </Layout>
   )
 }
--- src/Pages/Signup/index.jsx
+++ src/Pages/Signup/index.jsx
@@ -1,25 +1,22 @@
 import Layout from '../../Components/Layout'
-import { NavLink } from 'react-router-dom'
-function SignIn() {
+
+function SignUp() {
   return (
     <Layout>
       <div className='flex flex-col text-center w-72'>
         <div className='py-2 font-medium text-xl'>  
           Welcome
         </div>
+        <div className='text-left'>Your name:</div>
+        <input className='rounded-lg border-2 border-black p-2 text-sm mb-3' placeholder='Peter'></input>
         <div className='text-left'>Your email:</div>
         <input className='rounded-lg border-2 border-black  p-2 text-sm mb-3' placeholder='hi@helloworld.com'></input>
         <div className='text-left'>Your password:</div>
         <input className='rounded-lg border-2 border-black p-2 text-sm mb-3' type='password' placeholder='******'></input>
-        <div className='py-3 bg-black text-white cursor-pointer rounded-lg'>Log in</div>
-        <div className='py-4 cursor-pointer underline text-sm'>Forgot my password</div>
-        <NavLink
-            to='/sign-up'>
-        <div className='py-3 cursor-pointer rounded-lg border-2 border-black'>Sign Up</div>
-        </NavLink>
+        <div className='py-3 bg-black text-white cursor-pointer rounded-lg mt-2'>Create</div>
       </div>
     </Layout>
   )
 }
 
-export default SignIn
+export default SignUp
