36342ecb6015d4ce4e15808b2b4b59b619511539
Luis Valladares <ls.valladares7@gmail.com>
2023-09-14 15:42:53 -0600

    feat(1.13): maquetacion de pagina "MyAccount"

--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -1,9 +1,23 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 
 function MyAccount() {
+  const context = useContext(ShoppingCartContext);
+
   return (
     <Layout>
-      MyAccount
+      <div className='grid gap-6 w-80'>
+        <h1 className='text-center text-xl font-medium'>My account</h1>
+        <div className='text-sm font-light leading-6'>
+          <p>Name: <span className='text-base font-normal'>{context.user?.name}</span></p>
+          <p>Email: <span className='text-base font-normal'>{context.user?.email}</span></p>
+        </div>
+        <Link className='inline-block'>
+          <button className='w-full py-3 border rounded-md border-black'>Edit</button>
+        </Link>
+      </div>
     </Layout>
   )
 }
