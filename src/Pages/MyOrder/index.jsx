757c7c0f30ca61d488e0e45c87d9565d4e49587d
Luis Valladares <ls.valladares7@gmail.com>
2023-09-15 15:18:34 -0600

    feat(4.9): Responsive design para pagina "MyOrder"

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -13,24 +13,26 @@ function MyOrder() {
 
   return (
     <Layout>
-      <div className='flex items-center justify-center relative w-80 mb-6'>
-        <Link to='/my-orders' className='absolute left-0'>
-          <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer' />
-        </Link>
-        <h1>My Order</h1>
-      </div>
-      <div className='flex flex-col w-80'>
-        {
-          context.order?.[index]?.products.map(product => (
-            <OrderCard
-              key={product.id}
-              id={product.id}
-              title={product.title}
-              imageUrl={product.images}
-              price={product.price}
-            />
-          ))
-        }
+      <div className='w-full max-w-xs'>
+        <div className='flex items-center justify-center relative mb-6'>
+          <Link to='/my-orders' className='absolute left-0'>
+            <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer' />
+          </Link>
+          <h1>My Order</h1>
+        </div>
+        <div className='grid gap-y-3'>
+          {
+            context.order?.[index]?.products.map(product => (
+              <OrderCard
+                key={product.id}
+                id={product.id}
+                title={product.title}
+                imageUrl={product.images}
+                price={product.price}
+              />
+            ))
+          }
+        </div>
       </div>
     </Layout>
   )
