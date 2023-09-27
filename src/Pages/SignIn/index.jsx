1e9ba220385d0505d8c27b86affa7e393dd2ba95
wilgar <wilenri341@hotmail.com>
2023-09-27 16:33:17 -0500

    Reto 4: Agregar estado local para mostrar la vista de crear cuenta o de hacer log in, Done

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ShoppingCartContext } from "../../Context"
 import Layout from '../../Components/Layout'
@@ -7,6 +7,8 @@ function SignIn() {
 
   const context = useContext(ShoppingCartContext)
 
+  const [view, setView] =useState("user-info")
+
 
   // Account
   const account = localStorage.getItem("account")
@@ -14,39 +16,52 @@ function SignIn() {
   const parsedAccount = JSON.parse(account)
 
   // Has an account
-
   const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
   const noAccountInLocalStage = context.account ? Object.keys(context.account).length === 0 : true
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalStage
+
+  const renderLogIn = () => {
+    return (
+      <div className='flex flex-col w-80'>
+        <p>
+          <span className='font-light text-sm'>Email:</span>
+          <span>{parsedAccount?.email}</span>
+        </p>
+        <p>
+          <span className='font-light text-sm'>Password:</span>
+          <span>{parsedAccount?.password}</span>
+        </p>
+        <Link
+          to="/">
+          <button
+            className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'
+            disabled={!hasUserAnAccount}>  
+              Log in
+          </button>
+        </Link>
+        <div className='text-center'> 
+          <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
+        </div>
+        <button
+            className='border border-black disabled:text-black/40  disabled:border-black/40 rounded-lg py-3 mt-6'
+            onClick={() => setView("create-user-info")}
+            disabled={hasUserAnAccount}>
+              Sign up
+        </button>
+      </div>
+    )
+  }
+
+  const renderCreateUserInfo = () => {
+    //TODO: Create render view
+  }
+
+  const renderView = () => view === "create-user-info" ? renderCreateUserInfo() : renderLogIn()
+
   return (
     <Layout>
        <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
-       <div className='flex flex-col w-80'>
-          <p>
-            <span className='font-light text-sm'>Email:</span>
-            <span>{parsedAccount?.email}</span>
-          </p>
-          <p>
-            <span className='font-light text-sm'>Password:</span>
-            <span>{parsedAccount?.password}</span>
-          </p>
-          <Link
-            to="/">
-            <button
-              className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'
-              disabled={!hasUserAnAccount}>  
-                Log in
-            </button>
-          </Link>
-          <div className='text-center'> 
-            <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
-          </div>
-          <button
-              className='border border-black disabled:text-black/40  disabled:border-black/40 rounded-lg py-3 mt-6'
-              disabled={hasUserAnAccount}>
-                Sign up
-            </button>
-       </div>
+      {renderView()}
     </Layout>
   )
 }
